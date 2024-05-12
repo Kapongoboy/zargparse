e64379f0d9200853c0bdefcc3ec2ea14e12e4941
Plamedi Luis <plamedi.luis11@gmail.com>
2024-05-12 18:01:34 +0200

    trying to add storage capability

--- src/root.zig
+++ src/root.zig
@@ -7,6 +7,7 @@
 //! the works.
 
 const std = @import("std");
+const stdout = std.io.getStdOut().writer();
 
 /// The main primitive of the module, the ParserArg struct is used to add arguments
 /// to your programs argument parser
@@ -36,14 +37,23 @@ pub const ArgumentParser = struct {
         ArgumentTooLong,
     };
 
+    const Value = union {
+        str: []const u8,
+        num: i64,
+        flt: f64,
+        boolean: bool,
+    };
+
     arg_iter: std.process.ArgIterator,
     arg_table: std.StringHashMap(ParserArg),
+    program_store: std.StringHashMap(Value),
     ally: std.mem.Allocator,
 
     pub fn init(a: std.mem.Allocator) !ArgumentParser {
         return ArgumentParser{
             .arg_iter = try std.process.ArgIterator.initWithAllocator(a),
             .arg_table = std.StringHashMap(ParserArg).init(a),
+            .program_store = std.StringHashMap(Value).init(a),
             .ally = a,
         };
     }
@@ -61,7 +71,7 @@ pub const ArgumentParser = struct {
         }
     }
 
-    ///  TODO have to figure how to mutate the name without corrupting the argument
+    ///  Fixed memory issue, undecided whether returning index is the best type
     fn parseNameToMapKey(buffer: []u8, name: []const u8) !usize {
         var buffer_idx: usize = 0;
 
@@ -90,12 +100,19 @@ pub const ArgumentParser = struct {
         return buffer_idx;
     }
 
+    /// This probably needs some care
+    fn flagToString(str: []const u8) ![]u8 {
+        var buf: [512]u8 = undefined;
+        const len: usize = try parseNameToMapKey(&buf, str);
+        return buf[0..len];
+    }
+
     /// Adds an argument to the parser.
     /// Returns an Allocator.Error from the internal lookup table.
     /// Due to the current state of zig libraries, rather than ergonomic functions to build the ParserArg
     /// Struct, Instead a complete ParserArg struct is passed to the function.
     /// This lines up currently with the philosophy of the zig standard library but may change as some of the apis have.
-    pub fn add_argument(self: *Self, options: ParserArg) !void {
+    pub fn addArgument(self: *Self, options: ParserArg) !void {
         var buf: [512]u8 = undefined;
         const len: usize = try parseNameToMapKey(&buf, options.name);
         try self.arg_table.put(try self.ally.dupe(u8, buf[0..len]), options);
@@ -108,12 +125,52 @@ pub const ArgumentParser = struct {
         } else return ParserError.ArgumentNotFound;
     }
 
+    // fn checkFlag() bool {} // TODO will need when having unqualified values as an option
+
     /// MAJOR TODO, This function is the heart of the parser
     /// and will abstract away putting everything in the right place
     /// Still deciding on an optimal design for it's functionality;
     pub fn parseArgs(self: *Self) !void {
-        _ = self;
-        @compileError("This function still needs to be implemented \n");
+        while (self.arg_iter.next()) |arg| {
+            const arg_internal = self.get(try flagToString(arg)) catch |e| {
+                try stdout.print("Argument {s} was not found as valid for the program, use -h to get the valid argument, error {e}\n", .{ arg, e });
+                return;
+            };
+
+            if (std.mem.eql(u8, arg_internal.action, "store")) {
+                if (self.arg_iter.next()) |val| {
+                    switch (arg_internal.arg_type) {
+                        ParserArg.ArgType.STRING => {
+                            self.program_store.put(try self.ally.dupe(u8, try flagToString(arg_internal.name)), .{ .str = val });
+                        },
+                        ParserArg.ArgType.INT => {
+                            const num = try std.fmt.parseInt(i64, val, 10);
+                            self.program_store.put(try self.ally.dupe(u8, try flagToString(arg_internal.name)), .{ .num = num });
+                        },
+                        ParserArg.ArgType.FLOAT => {
+                            const flt = try std.fmt.parseFloat(f64, val);
+                            self.program_store.put(try self.ally.dupe(u8, try flagToString(arg_internal.name)), .{ .flt = flt });
+                        },
+                        ParserArg.ArgType.BOOL => {
+                            const result: bool = undefined;
+
+                            if (std.mem.eql(u8, val, "true") or std.mem.eql(u8, val, "yes") or std.mem.eql(u8, val, "1")) {
+                                result = true;
+                            } else if (std.mem.eql(u8, val, "false") or std.mem.eql(u8, val, "no") or std.mem.eql(u8, val, "0")) {
+                                result = false;
+                            } else {
+                                try stdout.print("Argument {s} requires a boolean value\n", .{arg});
+                                return;
+                            }
+                            self.program_store.put(try self.ally.dupe(u8, try flagToString(arg_internal.name)), .{ .boolean = result });
+                        },
+                    }
+                } else {
+                    try stdout.print("Argument {s} requires a value\n", .{arg});
+                    return;
+                }
+            }
+        }
     }
 };
 
@@ -121,7 +178,7 @@ test "ArgumentParser basic test" {
     var parser = try ArgumentParser.init(std.testing.allocator);
     defer parser.deinit();
 
-    try parser.add_argument(ParserArg{
+    try parser.addArgument(ParserArg{
         .name = "test",
         .help = "test help",
     });
@@ -136,7 +193,7 @@ test "ArgumentParser dash string" {
     var parser = try ArgumentParser.init(std.testing.allocator);
     defer parser.deinit();
 
-    try parser.add_argument(ParserArg{
+    try parser.addArgument(ParserArg{
         .name = "--test-dash",
         .help = "test help",
     });
